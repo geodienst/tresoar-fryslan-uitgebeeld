b54d27efeaac8abc459498d6b66322a5dcb7b9b3
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2016-02-18 18:00:00 +0100

    I probably should enable caching after seeing how many requests came through

--- proxy.php
+++ proxy.php
@@ -8,5 +8,16 @@ unset($_GET['url']);
 
 $url .= (strpos($url, '?') === false ? '?' : '&') . http_build_query($_GET);
 
-$fh = fopen($url, 'rb');
-fpassthru($fh);
+$cache_file = '/tmp/tresoarproxy/' . sha1($url);
+
+if (!file_exists($cache_file)) {
+	if (!is_dir(dirname($cache_file)))
+		mkdir(dirname($cache_file));
+
+	$response = file_get_contents($url);
+	file_put_contents($cache_file, $response);
+	echo $response;
+} else {
+	$fh = fopen($cache_file, 'rb');
+	fpassthru($fh);
+}
