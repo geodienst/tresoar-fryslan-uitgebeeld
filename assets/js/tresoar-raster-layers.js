1a1086e2e143d0b7ae972973135b4344bb9bd9e2
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2016-04-15 15:36:29 +0200

    Add bonnebladen, move configuration of sources to a separate file, use the groups configuration also for separating the feature layers from the raster layers instead of dedicated HTML.

--- assets/js/tresoar-raster-layers.js
+++ assets/js/tresoar-raster-layers.js
@@ -1,96 +1,120 @@
-$.ajax(window.TRESOAR_WMS_URL, {
-	type: 'GET',
-	data: {
-		service: 'wms',
-		version: '1.3.0',
-		request: 'GetCapabilities',
-		namespace: 'tresoar'
+window.loaders.WMS = function(url) {
+	var ns = {
+		wms: 'http://www.opengis.net/wms'
+	};
+	
+	function getChildNodesByTagNameNS(ns, parent, nodeName) {
+		return $.makeArray(parent.childNodes).filter(function(child) {
+			return child.nodeType == parent.ELEMENT_NODE
+				&& child.namespaceURI == ns
+				&& child.nodeName == nodeName;
+		});
 	}
-}).done(function(data) {
-	var wmsLayers = $(data).find('Layer > Layer').map(function() {
-		var $layer = $(this),
-			$bbox = $layer.find('BoundingBox[CRS="EPSG:28992"]');
 
-		var bbox = $bbox.length ? new ol.Feature({
-			typename: $layer.find('title').text(),
-			geometry: new ol.geom.Polygon([[
-				[$bbox.attr('minx'), $bbox.attr('miny')].map(parseFloat),
-				[$bbox.attr('maxx'), $bbox.attr('miny')].map(parseFloat),
-				[$bbox.attr('maxx'), $bbox.attr('maxy')].map(parseFloat),
-				[$bbox.attr('minx'), $bbox.attr('maxy')].map(parseFloat)
-			]], 'XY')
-		}) : null;
+	$.ajax(url, {
+		type: 'GET',
+		data: {
+			service: 'wms',
+			version: '1.3.0',
+			request: 'GetCapabilities',
+			namespace: 'tresoar'
+		}
+	}).done(function(data) {
 
-		// Skip data without bbox for now.
-		if (!bbox) return null;
+		var wmsLayers = $.makeArray(data.getElementsByTagNameNS(ns.wms, 'Layer')).map(function(layer) {
+			var $layer = $(layer);
 
-		var extent = [
-			$bbox.attr('minx'), $bbox.attr('miny'),
-			$bbox.attr('maxx'), $bbox.attr('maxy')].map(parseFloat);
+			var $bbox = getChildNodesByTagNameNS(ns.wms, layer, 'BoundingBox').find(function(el) {
+				return el.hasAttribute('CRS') && el.getAttribute('CRS') == 'EPSG:28992';
+			});
 
-		// Ugly side-effect implementation
-		window.layers[$layer.children('Name').text()] = new ol.layer.Tile({
-			id: $layer.children('Name').text(),
-			name: $layer.children('Title').text(),
-			extent: extent,
-			bbox: bbox,
-			thumbnail: window.TRESOAR_WMS_URL + '?' + jQuery.param({
-				service: 'wms',
-				version: '1.3.0',
-				request: 'getMap',
-				format: 'image/png',
-				transparent: true,
-				styles: '',
-				layers: $layer.children('Name').text(),
-				srs: 'EPSG:28992',
-				width: 300,
-				height: 300,
-				bbox: bbox.getGeometry().getExtent().join(',')
-			}),
-			source: new ol.source.TileWMS({
-				url: TRESOAR_WMS_URL,
-				serverType: 'geoserver',
-				params: {
-					LAYERS: $layer.children('Name').text(),
-					TILED: true	
-				},
-				projection: 'EPSG:28992'
+			var bbox = $bbox ? new ol.Feature({
+				//typename: $layer.find('title').text(),
+				geometry: new ol.geom.Polygon([[
+					[$bbox.getAttribute('minx'), $bbox.getAttribute('miny')].map(parseFloat),
+					[$bbox.getAttribute('maxx'), $bbox.getAttribute('miny')].map(parseFloat),
+					[$bbox.getAttribute('maxx'), $bbox.getAttribute('maxy')].map(parseFloat),
+					[$bbox.getAttribute('minx'), $bbox.getAttribute('maxy')].map(parseFloat)
+				]], 'XY')
+			}) : null;
+
+			// Skip data without bbox for now.
+			if (!bbox) return null;
+
+			var extent = [
+				$bbox.getAttribute('minx'), $bbox.getAttribute('miny'),
+				$bbox.getAttribute('maxx'), $bbox.getAttribute('maxy')].map(parseFloat);
+
+			var name = getChildNodesByTagNameNS(ns.wms, layer, 'Name')[0].textContent;
+			var title = getChildNodesByTagNameNS(ns.wms, layer, 'Title')[0].textContent;
+
+			// Ugly side-effect implementation
+			window.layers[name] = new ol.layer.Tile({
+				id: name,
+				name: title,
+				extent: extent,
+				bbox: bbox,
+				thumbnail: url + '?' + jQuery.param({
+					service: 'wms',
+					version: '1.3.0',
+					request: 'getMap',
+					format: 'image/png',
+					transparent: true,
+					styles: '',
+					layers: name,
+					crs: 'EPSG:28992',
+					srs: 'EPSG:28992',
+					width: 300,
+					height: 300,
+					bbox: bbox.getGeometry().getExtent().join(',')
+				}),
+				source: new ol.source.TileWMS({
+					url: url,
+					serverType: 'geoserver',
+					params: {
+						LAYERS: name,
+						TILED: true	
+					},
+					projection: 'EPSG:28992'
+				}),
+				style: new ol.style.Style({
+					stroke: new ol.style.Stroke({
+						color: 'green',
+						width: 2
+					})
+				})
+			});
+
+			return bbox;
+		});
+
+		// Add a debug map that contains all the extents of the just fetched WFS
+		/*
+		window.layers['GeoServerWMS'] = new ol.layer.Vector({
+			id: 'GeoServerWMS',
+			name: '#Geoserver WMS',
+			source: new ol.source.Vector({
+				features: wmsLayers,
+				strategy: ol.loadingstrategy.tile(ol.tilegrid.createXYZ({
+					maxZoom: 19
+				})),
+				projection: 'EPSG:28992',
+				attributions: [new ol.Attribution({
+					html: 'Geoserver bboxes'
+				})],
 			}),
 			style: new ol.style.Style({
 				stroke: new ol.style.Stroke({
 					color: 'green',
 					width: 2
+				}),
+				fill: new ol.style.Fill({
+					color: [0, 0, 255, 0.6]
 				})
 			})
 		});
+		*/
 
-		return bbox;
-	}).get();
-
-	// Add a debug map that contains all the extents of the just fetched WFS
-	window.layers['GeoServerWMS'] = new ol.layer.Vector({
-		id: 'GeoServerWMS',
-		name: '#Geoserver WMS',
-		source: new ol.source.Vector({
-			features: wmsLayers,
-			strategy: ol.loadingstrategy.tile(ol.tilegrid.createXYZ({
-				maxZoom: 19
-			})),
-			projection: 'EPSG:28992',
-			attributions: [new ol.Attribution({
-				html: 'Geoserver bboxes'
-			})],
-		}),
-		style: new ol.style.Style({
-			stroke: new ol.style.Stroke({
-				color: 'green',
-				width: 2
-			}),
-			fill: new ol.style.Fill({
-				color: [0, 0, 255, 0.6]
-			})
-		})
+		updateLayerList();
 	});
-
-	updateLayerList();
-});
+};
