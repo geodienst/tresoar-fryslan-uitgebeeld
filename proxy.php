e2270dad03ad7cc15bdf6da10473eaa71a6ae82a
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2016-04-19 12:10:06 +0200

    Add a whitelist to the proxy script

--- proxy.php
+++ proxy.php
@@ -1,30 +1,40 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: ' . $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']);
 header('Access-Control-Allow-Methods: GET, OPTIONS');
 header('Access-Control-Max-Age: 1728000');
 
-if (!isset($_GET['url']))
+$allowed = [
+	'{^http://geoportaal\.fryslan\.nl/arcgis/services/ProvinciaalGeoRegister/PGR_features/GeoDataServer/WFSServer.+?}'
+];
+
+function is_whitelisted($url, $whitelist) {
+	foreach ($whitelist as $pattern)
+		if (preg_match($pattern, $url))
+			return true;
+	return false;
+}
+
+if (!isset($_GET['url'])) {
+	header('HTTP/1.0 400 Bad Request');
 	die('url parameter missing');
+}
 
 $url = $_GET['url'];
 unset($_GET['url']);
 
-$url .= (strpos($url, '?') === false ? '?' : '&') . http_build_query($_GET);
+$url = $url . (strpos($url, '?') === false ? '?' : '&') . http_build_query($_GET);
 
-$cache_file = '/tmp/tresoarproxy/' . sha1($url);
-
-$nocache = true;
-
-if (!file_exists($cache_file) || $nocache) {
-	if (!$nocache) 
-		if (!is_dir(dirname($cache_file)))
-			mkdir(dirname($cache_file));
-
-	$response = file_get_contents($url);
-	if (!$nocache) file_put_contents($cache_file, $response);
-	echo $response;
-} else {
-	$fh = fopen($cache_file, 'rb');
-	fpassthru($fh);
+if (!is_whitelisted($url, $allowed)) {
+	header('HTTP/1.0 403 Forbidden');
+	die('The requested URL is not whitelisted in the proxy script');
 }
+
+if (!($fh = @fopen($url, 'rb'))) {
+	header('HTTP/1.0 500 Internal Server Error');
+	$error = error_get_last();
+	die($error['message']);
+}
+
+fpassthru($fh);
+fclose($fh);
