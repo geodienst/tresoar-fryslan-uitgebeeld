a0f72a477960b0dd8cce91d0d33fb3b90b3b492c
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2018-07-28 18:12:17 +0200

    Update Geocoder widget to use PDOK’s location server instead of the geocoded

--- assets/js/pdok-geocoder.js
+++ assets/js/pdok-geocoder.js
@@ -2,89 +2,27 @@ var pdok = new Bloodhound({
 	datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
   	queryTokenizer: Bloodhound.tokenizers.whitespace,
 	remote: {
-		url: 'http://geodata.nationaalgeoregister.nl/geocoder/Geocoder',
+		url: 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest',
 		prepare: function(query, settings) {
 			settings.type = 'GET';
-			settings.dataType = 'xml';
+			settings.dataType = 'json';
 			settings.data = {
-				zoekterm: query,
-				strict: false
+				q: query
 			};
 			return settings;
 		},
-		transform: function(xmlDoc) {
-			var ns = {
-				xls: 'http://www.opengis.net/xls',
-				gml: 'http://www.opengis.net/gml'
-			};
+		transform: function(json) {
+			var suggestions = json.response.docs;
 
-			var adressen = [];
-
-			var geocodedAdres = xmlDoc.getElementsByTagNameNS(ns.xls, "GeocodedAddress");
-	
-			for (var i=0; i < geocodedAdres.length;i++) {
-				var adres = {
-					straat: '',
-					huisnummer: '',
-					huisletter: '',
-					postcode: '',
-					plaats: '',
-					provincie: '',
-					name: '',
-					pos: ''
-				}
-
-				adres.pos = geocodedAdres[i].getElementsByTagNameNS(ns.gml, "pos")[0].childNodes[0].nodeValue;
-				if (geocodedAdres[i].getElementsByTagNameNS(ns.xls, "Street").length > 0) {
-					adres.straat = geocodedAdres[i].getElementsByTagNameNS(ns.xls, "Street")[0].childNodes[0].nodeValue;	
-					
-					if (geocodedAdres[i].getElementsByTagNameNS(ns.xls, "Building").length > 0){
-						adres.huisnummer = geocodedAdres[i].getElementsByTagNameNS(ns.xls, "Building")[0].getAttribute("number");
-						if (geocodedAdres[i].getElementsByTagNameNS(ns.xls, "Building")[0].getAttribute("subdivision")) {
-								adres.huisletter = geocodedAdres[i].getElementsByTagName(ns.xls, "Building")[0].getAttribute("subdivision");
-						}
-					}
-
-					if (geocodedAdres[i].getElementsByTagNameNS(ns.xls, "PostalCode").length > 0){
-						adres.postcode = geocodedAdres[i].getElementsByTagNameNS(ns.xls, "PostalCode")[0].childNodes[0].nodeValue;	
-					}
-				}
-
-				var places = geocodedAdres[i].getElementsByTagNameNS(ns.xls, "Place")
-				for (j=0;j < places.length;j++){
-					if (places[j].getAttribute("type") == "MunicipalitySubdivision") {
-						adres.plaats = places[j].childNodes[0].nodeValue;
-					}
-					if (places[j].getAttribute("type") == "CountrySubdivision") {
-						adres.provincie = places[j].childNodes[0].nodeValue;
-					}
+			suggestions.forEach(function(suggestion) {
+				suggestion.fetch = function(callback) {
+					$.getJSON('https://geodata.nationaalgeoregister.nl/locatieserver/v3/lookup?', {id: suggestion.id}, function(data) {
+						callback(data.response.docs[0]);
+					});
 				};
+			})
 
-				var nonEmpty = function(inp) {
-					return inp != '';
-				}
-
-				adres.name = [
-					[adres.straat, adres.huisnummer, adres.huisletter].filter(nonEmpty).join(' '),
-					[adres.postcode, adres.plaats].filter(nonEmpty).join(' '),
-					[adres.provincie].join(' ')
-				].filter(nonEmpty).join(', ');
-
-				if (adres.huisnummer)
-					adres.niveau = 'huisnummer';
-				else if (adres.straat)
-					adres.niveau = 'straat';
-				else if (adres.plaats)
-					adres.niveau = 'plaats';
-				else if (adres.provincie)
-					adres.niveau = 'provincie';
-				else
-					adres.niveau = 'onbekend';
-
-				adressen.push(adres);
-			}
-
-			return adressen;
+			return suggestions;
 		}
 	}
 })
@@ -94,6 +32,6 @@ $('input[data-provider="pdok-geocoder"]').typeahead({
 	minLength: 3
 }, {
 	name: 'pdok-geocoder',
-	display: 'name',
+	display: 'weergavenaam',
 	source: pdok
-});
+})
